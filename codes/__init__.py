1ce6a28c628cb4a46a0136b2174397e735dcb4d6
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-22 03:49:56 +0300

    z

---
+++ codes/__init__.py
@@ -0,0 +1,12 @@
+
+from .ai_studio_code import remove_footer_and_resize as ai_studio_1
+from .ai_studio_code2 import remove_footer_and_resize as ai_studio_2
+from .remove_svg_footer import remove_footer_and_adjust_height as claude_1
+from .remove_svg_footer2 import remove_footer_and_adjust_height as claude_2
+
+__all__ = [
+    "ai_studio_1",
+    "ai_studio_2",
+    "claude_1",
+    "claude_2",
+]
